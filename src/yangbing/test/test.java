26baf291980e981604ff5f32aa2749e6e2feb2eb
YangBing <yb1995@foxmail.com>
2017-05-12 10:50:21 +0800

    modify test.java

--- src/yangbing/test/test.java
+++ src/yangbing/test/test.java
@@ -12,7 +12,7 @@ public class test {
 	public static void main(String[] args) throws Exception {
 		IStudentDao dao = new IStudentDaoImp();
 		Student student = new Student("杨兵", 22, 100);
-		//System.out.println(dao.insertStu(student));
+		System.out.println(dao.insertStu(student));
 		List<Student> s = dao.selectAllStudent();
 		for(int i = 0 ; i < s.size() ; i++)
 		{
